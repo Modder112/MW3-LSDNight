bdf49781f016ed302b822b211e57da157de906e5
Modder112 <hiho-jo@web.de>
2018-08-01 19:04:59 +0200

    Uploaded
    I have uploaded this files!

---
+++ nightv2/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using InfinityScript;
+
+namespace nightv2
+{
+    public class nightv2 : BaseScript
+    {
+
+        public nightv2()
+            : base()
+        {
+            base.PlayerConnected += PlayerConnect;
+        }
+
+        public void PlayerConnect(Entity Player)
+        {
+            Player.SpawnedPlayer += new Action(() =>
+            {
+                base.OnInterval(50, () => this.Vision(Player, "cobra_sunset3", false));
+            });
+        }
+
+        public bool Vision(Entity player, string vision, bool thermal)
+        {
+            if (thermal)
+            {
+                player.Call("ThermalVisionOn", new Parameter[0]);
+            }
+            else
+            {
+                player.Call("visionsetnakedforplayer", new Parameter[]
+				{
+					vision,
+					5
+				});
+            }
+            return true;
+        }
+    }
+}
